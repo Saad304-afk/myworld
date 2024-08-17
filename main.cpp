316abaac73713c567735e3163e0daef44c76a26d
Saad <saadbinmontasir11@gmail.com>
2024-08-17 20:14:25 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+
+
+{
+
+
+double height,base,area;
+
+cout<<"enter the height";
+cin>> height;
+
+cout<<" enter the base";
+cin>>base;
+area=1.0/2*height*base;
+
+
+cout<<"the area of triangfle is ="<<area<<endl;
+
+    return(0);
+}
